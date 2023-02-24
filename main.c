15d60a544b96918c45fcd199dba9a6c871b856c2
Thatagricguy <samueljohn3999@gmail.com>
2023-02-24 14:47:56 -0800

    shell

--- main.c
+++ main.c
@@ -5,14 +5,25 @@ int main(int ac, char **argv)
 	char *prompt = "(Myshell) $ ";
 	char *lineptr;
 	size_t n = 0;
+	ssize_t nchars_read;
 
 	/* declaring void variables */
 	(void)ac; (void)argv;
-
+	
+	while(1)
+	{
 	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
+	nchars_read = getline(&lineptr, &n, stdin);
+	/* check if the geline function failed or reached EOF or user pressed ctl D*/
+	if (nchars_read == -1)
+	{
+		printf("Exiting shell ...\n");
+		return (-1);
+	}
 	printf("%s\n", lineptr);
-
+	
+	/* free up allocated memery */
 	free(lineptr);
+	}
 	return (0);
 }
